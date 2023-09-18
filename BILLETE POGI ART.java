6aef395b7b3a495eae2a9a8b5931193fa47b446c
JinHisao <145302239+JinHisao@users.noreply.github.com>
2023-09-18 08:17:57 +0800

    Add files via upload

---
+++ BILLETE POGI ART.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.art;
+
+/**
+ *
+ * @author LAB2-PC7
+ */
+public class ART {
+
+    public static void main(String[] args) {
+System.out.println("　　　                AAAAAA                            ");
+System.out.println("                    AAAAAAAA                           ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                IIAAAAAAAAAAAAII                       ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                   AAAAAAAAAA                          ");
+System.out.println("                  BBBBBBBBBBBB                         ");
+System.out.println("               M BBBBBBBBBBBBBB M                     ");
+System.out.println("               MBBBBBBBBBBBBBBBBM                      ");
+System.out.println("              NNBBBBBBBBBBBBBBBBNN                     ");
+System.out.println("              NNNNNIGGGGGGGAAAAAAAA                    ");
+System.out.println("              NNNNNIIIIIGGEERRRRRRR                    ");
+System.out.println("              LLLLLL         LLLLLL                    ");
+System.out.println("              LLLLLL         LLLLLL                    ");
+System.out.println("              LLLLLL         LLLLLL                    ");
+System.out.println("              LLLLLL         LLLLLL                    ");
+System.out.println("              LLLLLLNNNNNNNNNLLLLLL                    ");
+System.out.println("                    SSSSSSSSS                          ");
+System.out.println("                     SSS SSS                           ");
+System.out.println("_______________________________________________________");
+    }
+} 
